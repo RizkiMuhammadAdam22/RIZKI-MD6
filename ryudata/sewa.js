06481bfe813a6affbe52c9aa7e2b9300ec2ca0c2
RizkiMuhammadAdam22 <107252712+RizkiMuhammadAdam22@users.noreply.github.com>
2022-06-11 07:01:36 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -3,13 +3,13 @@ exports.sewa = () => {
 *SEWA BOT 💫*
 
 📆 1 Minggu : Rp. 10.000,00
-📆 1 Bulan : Rp. 20.000,00
+📆 1 Bulan : Rp. 25.000,00
 📆 1 Tahun : Rp. 100.000,00
 📆 Permanen : Buat Bot Sendiri!!!
 
 *🏦 PAYMENT :*
 - Gopay
 - Dana
-- Paypal
+- Pulsa
 `
 }
